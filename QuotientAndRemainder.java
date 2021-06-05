bb1da52b56e5bf780c28689191770600d25cd438
Naveen Kumar S <42862879+NaveenKumar0612@users.noreply.github.com>
2021-06-05 17:07:30 +0530

    Create QuotientAndRemainder.java

---
+++ QuotientAndRemainder.java
@@ -0,0 +1,11 @@
+package source.code;
+
+public class QuotientAndRemainder {
+	public static void main(String[] args) {
+		int x = 100, y = 6;
+		int quotient = x / y;
+		int remainder = x % y;
+		System.out.println("Quotient is:" + quotient);
+		System.out.println("Remainder is:" + remainder);
+	}
+}
