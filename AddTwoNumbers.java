4e6afb6472409690af83cb9345d664729bd1b04d
Naveen Kumar S <42862879+NaveenKumar0612@users.noreply.github.com>
2021-06-05 17:05:46 +0530

    Delete AddTwoNumbers.java

--- AddTwoNumbers.java
+++
@@ -1,9 +0,0 @@
-package source.code;
-
-public class AddTwoNumbers {
-	public static void main(String[] args) {
-		int x = 100, y = 150, z;
-		z = x + y;
-		System.out.println("sum of these numbers: "+z);
-	}
-}
