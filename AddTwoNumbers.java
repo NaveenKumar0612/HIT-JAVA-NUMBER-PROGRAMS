e96ffeb8d8ff1d50e38dbaf3cb9f7156c4c74291
Naveen Kumar S <42862879+NaveenKumar0612@users.noreply.github.com>
2021-06-05 17:07:03 +0530

    Create AddTwoNumbers.java

---
+++ AddTwoNumbers.java
@@ -0,0 +1,9 @@
+package source.code;
+
+public class AddTwoNumbers {
+	public static void main(String[] args) {
+		int x = 100, y = 150, z;
+		z = x + y;
+		System.out.println("sum of these numbers: "+z);
+	}
+}
