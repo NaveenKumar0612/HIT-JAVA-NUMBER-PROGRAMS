e7d662deaafbae2fa20c19cf1d6f908f94bbeacb
Naveen Kumar S <42862879+NaveenKumar0612@users.noreply.github.com>
2021-06-05 17:09:17 +0530

    Create CheckEvenOdd.java

---
+++ CheckEvenOdd.java
@@ -0,0 +1,11 @@
+package source.code;
+
+public class CheckEvenOdd {
+	public static void main(String[] args) {
+		int x = 200;
+	if(x % 2 == 0)
+		System.out.println("The Number is even");
+	else
+		System.out.println("The Number is odd");
+	}
+}
